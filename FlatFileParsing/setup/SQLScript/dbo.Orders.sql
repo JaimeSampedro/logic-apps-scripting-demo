fe2af893347ddeac299d62462f5c6ec349202300
Rajesh Ramabathiran <rajram@microsoft.com>
2015-05-28 15:20:57 -0700

    Demo script for flat file parsing
    Demo script for flat file parsing

---
+++ FlatFileParsing/setup/SQLScript/dbo.Orders.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Orders] (
+    [ID]        INT          IDENTITY (1, 1) NOT NULL,
+    [ItemName]  VARCHAR (50) NULL,
+    [Quantity]  INT          NULL,
+    [UnitPrice] FLOAT (53)   NULL,
+    [IsRead]    BIT          NULL,
+    PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
